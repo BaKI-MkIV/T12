bf2f391ffc512493c85539dd9b41d598d6450dac
BaKI <jackeandco@gmail.com>
2024-05-10 02:15:19 +0600

    It can be seen

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,18 +30,35 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 400);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(800, 50);
+            this.label1.TabIndex = 0;
+            this.label1.Text = " ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Location = new System.Drawing.Point(15, 15);
             this.Name = "Form1";
-            this.Text = "T12, будь он неладен";
+            this.Text = "T12";
             this.ResumeLayout(false);
         }
+
+        private System.Windows.Forms.Label label1;
 
         #endregion
     }
--- Form1.cs
+++ Form1.cs
@@ -1,41 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
 
 namespace T12
 {
     public partial class Form1 : Form
     {
-        private TextBox textBox1; 
+        private string previus = "";
         private int keyCount = 12;
+        private int currentIndex = 0; // переменная для отслеживания текущего индекса символа
+        private int ch = 0;
+        private Timer timer; // Таймер для отслеживания времени
+        private bool timerStarted = false; // Флаг для отслеживания состояния таймера
+        private TextBox outputTextBox; // Текстовое поле для вывода совпадений
+        private List<string> check = Loader("C:\\Users\\jacke\\Documents\\riderProject\\T12\\ruscorpora_content.xlsx");
 
         public Form1()
         {
             InitializeComponent();
-            CreateKeyboard(); 
-            CreateTextBox(); 
+
+            // Инициализируем таймер
+            timer = new Timer();
+            timer.Interval = 1000; // Интервал таймера в миллисекундах (1 секунда)
+            timer.Tick += Timer_Tick; // Обработчик события таймера
+
+            // Инициализация текстового поля вывода
+            outputTextBox = new TextBox();
+            outputTextBox.Multiline = true;
+            outputTextBox.ReadOnly = true;
+            outputTextBox.ScrollBars = ScrollBars.Vertical; // Включаем вертикальную прокрутку
+            outputTextBox.Dock = DockStyle.Right; // Докаем текстовое поле справа
+            outputTextBox.Width = 200; // Устанавливаем ширину текстового поля
+            outputTextBox.Font = new Font("Times New Roman", 14);
+            Controls.Add(outputTextBox);
+            CreateKeyboard();
         }
 
-        private void CreateTextBox()
-        {
-            textBox1 = new TextBox();
-            textBox1.Multiline = true;
-            textBox1.Dock = DockStyle.Bottom;
-            Controls.Add(textBox1);
-        }
 
         private void CreateKeyboard()
         {
             string alph = "АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
 
             List<string> alph_blocks = new List<string>();
-            
+
             for (int i = 0; i < alph.Length; i += 3)
             {
                 // Определяем длину подстроки (не превышающую 3 символа)
@@ -47,55 +58,170 @@ namespace T12
             }
 
             int x = 20;
-            int y = 20; 
-            int buttonWidth = 90; 
-            int buttonHeight = 50; 
+            int y = 20;
+            int buttonWidth = 100;
+            int buttonHeight = 60;
 
             for (int i = 0; i < alph_blocks.Count; i++)
             {
                 SelfButton button = new SelfButton(alph_blocks[i]);
                 button.Text = alph_blocks[i];
+
                 button.Size = new System.Drawing.Size(buttonWidth, buttonHeight);
                 button.Location = new System.Drawing.Point(x, y);
-                button.Click += Button_Click;
+                button.Click += button_Click;
                 Controls.Add(button);
-                
+
                 if ((i + 1) % 3 == 0)
                 {
                     x = 20;
-                    y += buttonHeight + 10;
+                    y += buttonHeight + 15;
                 }
                 else
                 {
-                    x += buttonWidth + 10;
+                    x += buttonWidth + 15;
                 }
             }
-            
+
             SelfButton space = new SelfButton(" ");
             space.Text = "Space";
             space.Size = new System.Drawing.Size(buttonWidth, buttonHeight);
             space.Location = new System.Drawing.Point(x, y);
-            space.Click += Button_Click; 
+            space.Click += button_Click;
             Controls.Add(space);
-            
-            
+
+
         }
 
-        private void Button_Click(object sender, EventArgs e)
+        private void button_Click(object sender, EventArgs e)
         {
-            SelfButton button = (SelfButton)sender; 
-            textBox1.Text += button.codeText; 
+            SelfButton button = (SelfButton)sender;
+
+            if (previus == "")
+            {
+                previus = button.Text;
+            }
+
+            if (button.Text != previus)
+            {
+                timer.Stop(); // Остановка таймера
+                timerStarted = false;
+                Timer_Tick(timer, EventArgs.Empty);
+            }
+
+
+            // Проверяем, чтобы индекс не выходил за пределы длины текста кнопки
+            if (currentIndex >= button.Text.Length)
+            {
+                currentIndex = 0;
+            }
+
+            // Обновляем текст метки
+            UpdateLabel(button);
+
+            // Сброс таймера до нуля и запуск, если не был запущен
+            timer.Stop();
+            timerStarted = false;
+            timer.Start();
+            timerStarted = true;
+
+            currentIndex++;
+            previus = button.Text;
+        }
+        
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            Pip(check, Crop(label1.Text));
+            currentIndex = 0;
+            label1.Text += " ";
+            ch++;
+            
+            timer.Stop();
+            timerStarted = false;
+            previus = "";
+        }
+
+        private void UpdateLabel(object sender)
+        {
+            SelfButton button = (SelfButton)sender; // Явное приведение объекта sender к типу SelfButton
+
+            if (button.Text != "Space")
+            {
+                string labelText = label1.Text; // Получаем текущий текст метки
+                labelText = labelText.Remove(ch, 1)
+                    .Insert(ch, button.codeText[currentIndex].ToString()); // Заменяем символ в указанной позиции
+                label1.Text = labelText;
+            }
+        }
+        
+        private static List<string> Loader(string filepath)
+        {
+            List<string> words = new List<string>();
+
+            try
+            {
+                using (FileStream file = new FileStream(filepath, FileMode.Open, FileAccess.Read))
+                {
+                    IWorkbook workbook = new XSSFWorkbook(file);
+                    ISheet sheet = workbook.GetSheetAt(0);
+                    
+                    for (int row = 1; row <= sheet.LastRowNum; row++)
+                    {
+                        IRow currentRow = sheet.GetRow(row);
+                        if (currentRow != null)
+                        {
+                            string word = currentRow.GetCell(1).StringCellValue.ToLower(); 
+                            words.Add(word);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при загрузке слов из файла: {ex.Message}");
+            }
+
+            return words;
+        }
+
+        private string Crop(string focus)
+        {
+            
+            int lastSpaceIndex = focus.LastIndexOf(' ');
+            if (lastSpaceIndex == -1)
+            {
+                return focus;
+            }
+            
+            return focus.Substring(lastSpaceIndex + 1);
+        }
+        
+        private void Pip(List<string> focus, string target)
+        {
+            outputTextBox.Clear();
+            if (target != "")
+            {
+                foreach (var it in focus)
+                {
+                    if (it.StartsWith(target.ToLower()))
+                    {
+                        AddMatch(it);
+                    }
+                }
+            }
+        }
+        
+        private void AddMatch(string match)
+        {
+            // Добавляем новое совпадение в текстовое поле
+            outputTextBox.AppendText(match + Environment.NewLine); // Добавляем совпадение и переходим на новую строку
         }
     }
     
     public class SelfButton : Button
     {
         public string codeText { get; set; }
-
-        public SelfButton() : base()
-        {
-            codeText = "Default Text";
-        }
 
         public SelfButton(string txt) : base()
         {
